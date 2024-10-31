8a46226fccfaeaf4f4b2d66a4c392e28eee11e64
apc-ka-abe <apc-ka-abe>
2024-10-31 09:47:30 +0000

    Fix Access semantic layer

--- databricks/Access_semantic_layer.py
+++ databricks/Access_semantic_layer.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
 # MAGIC %pip install dbt-sl-sdk[sync]
+# MAGIC %pip install --upgrade dbt-sl-sdk
 
 # COMMAND ----------
 
@@ -7,13 +8,21 @@ dbutils.library.restartPython()
 
 # COMMAND ----------
 
+import os
+os.environ["EVIROMENT_ID"] = dbutils.secrets.get(scope="dbt_demo", key = "eviroment_id")
+os.environ["SERVICE_TOKEN"] = dbutils.secrets.get(scope="dbt_demo", key = "service_token")
+os.environ["SEMANTIC_LAYER_HOST"] = dbutils.secrets.get(scope="dbt_demo", key = "host_name")
+
+# COMMAND ----------
+
 from dbtsl import SemanticLayerClient
 
 client = SemanticLayerClient(
-    environment_id=xxx,
-    auth_token="xxx",
-    host="semantic-layer.cloud.getdbt.com",
+    environment_id=os.getenv("EVIROMENT_ID"),
+    auth_token=os.getenv("SERVICE_TOKEN"),
+    host=os.getenv("SEMANTIC_LAYER_HOST")
 )
+# client.start_session()  # セッションを開始
 
 # COMMAND ----------
 
@@ -33,5 +42,5 @@ def main():
         print(pandas_df)
 
         spark_df = spark.createDataFrame(pandas_df)
-        spark_df.write.format("delta").saveAsTable("ka_abe.sample.checking_table")
+        spark_df.write.format("delta").mode("overwrite").saveAsTable("ka_abe.sample.checking_table")
 main()
